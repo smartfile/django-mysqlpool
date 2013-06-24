2bacd043e6361c95e4b7a826aa2e12853683fe3e
Ben Timby <btimby@gmail.com>
2013-06-24 14:17:18 -0400

    Don't import removed close_pool\(\).

--- django_mysqlpool/__init__.py
+++ django_mysqlpool/__init__.py
@@ -1,7 +1,5 @@
 from functools import wraps
 
-from django_mysqlpool.backends.mysqlpool.base import close_pool
-
 
 def auto_close_db(f):
     "Ensures the database connection is closed when the function returns."
